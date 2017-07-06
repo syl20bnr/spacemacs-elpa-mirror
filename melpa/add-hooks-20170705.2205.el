5a68a156ad69bf047e6078aac6ee3a86499da9cb
Golem <golem@d12frosted.io>
2017-07-06 12:30:05 +0000

    snapshot 06/07/17 12:30:05

--- melpa/add-hooks-20170705.2205.el
+++ melpa/add-hooks-20170705.2205.el
@@ -4,8 +4,8 @@
 
 ;; Author: Nick McCurdy <nick@nickmccurdy.com>
 ;; Created: 22 Jan 2017
-;; Version: 3.0.0
-;; Package-Version: 20170518.209
+;; Version: 3.1.0
+;; Package-Version: 20170705.2205
 ;; Keywords: lisp
 ;; Homepage: https://github.com/nickmccurdy/add-hooks
 
@@ -38,8 +38,11 @@
 ;;; Code:
 
 (defun add-hooks-listify (object)
-  "If OBJECT is a list, return it, else wrap it in a list."
-  (if (listp object) object (list object)))
+  "If OBJECT is a list and not a function, return it, else wrap it in a list."
+  (if (and (listp object)
+           (not (functionp object)))
+      object
+    (list object)))
 
 (defun add-hooks-normalize-hook (hook)
   "If HOOK is a symbol, ensure `-hook' is appended, else return HOOK itself."
@@ -52,11 +55,11 @@
 (defun add-hooks-pair (hooks functions)
   "Call `add-hook' for each combined pair of items in HOOKS and FUNCTIONS.
 
-Either value can be a single symbol or a list of symbols, in
-which case a function can be added to multiple hooks and/or
-multiple functions can be added to a hook.  This behaves like
-`add-hook' when both values are atoms.  It is implied that hook
-symbols will end with `-hook'.
+HOOKS can be a symbol or a list of symbols representing hook
+variables (the `-hook' suffix is implied).  FUNCTIONS can be a
+symbol, a lambda, or a list of either representing hook
+functions.  If lists are used, a function can be added to
+multiple hooks and/or multiple functions can be added to a hook.
 
 Example:
 
@@ -75,9 +78,8 @@ Example:
   "Call `add-hooks-pair' on each cons pair in PAIRS.
 
 Each pair has a `car' for setting hooks and a `cdr' for setting
-functions to add to those hooks.  Either side of the pair can be
-a single symbol or a list of symbols, as passed to
-`add-hooks-pair'.
+functions to add to those hooks.  Pair values are passed to the
+HOOKS and FUNCTIONS arguments of `add-hooks-pair', respectively.
 
 Usage:
 
