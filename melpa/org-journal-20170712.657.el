5a43a6ec718f93ed14925eb69a2386a7220b8ffd
Golem <golem@d12frosted.io>
2017-07-12 20:30:03 +0000

    snapshot 12/07/17 20:30:03

--- melpa/org-journal-20170712.657.el
+++ melpa/org-journal-20170712.657.el
@@ -2,7 +2,7 @@
 
 ;; Author: Bastian Bechtold
 ;; URL: http://github.com/bastibe/org-journal
-;; Package-Version: 20170705.217
+;; Package-Version: 20170712.657
 ;; Version: 1.12.1
 
 ;;; Commentary:
@@ -272,8 +272,7 @@ Whenever a journal entry is created the
       (funcall org-journal-find-file entry-path))
     (org-journal-decrypt)
     (goto-char (point-max))
-    (let ((unsaved (buffer-modified-p))
-          (new-file-p (equal (point-max) 1)))
+    (let ((new-file-p (equal (point-max) 1)))
 
       ;; empty file? Add a date timestamp
       (when new-file-p
@@ -310,9 +309,7 @@ Whenever a journal entry is created the
 
       ;; open the recent entry when the prefix is given
       (when should-add-entry-p
-        (show-entry))
-
-      (set-buffer-modified-p unsaved))))
+        (show-entry)))))
 
 (defun org-journal-carryover ()
   "Moves all items matching org-journal-carryover-items from the
--- stable-melpa/rufo-0.3.0.el
+++ stable-melpa/rufo-0.3.0.el
@@ -33,9 +33,9 @@
 
 ;; Author: Daniel Ma <danielhgma@gmail.com> and contributors
 ;; URL: https://github.com/danielma/rufo.el
-;; Version: 0.2.0
-;; Package-Version: 20170708.2242
-;; Package-X-Original-Version: 0.2.0
+;; Version: 0.3.0
+;; Package-Version: 0.3.0
+;; Package-X-Original-Version: 0.3.0
 ;; Package-Requires: ((emacs "24.3"))
 
 ;;; Commentary:
@@ -166,6 +166,10 @@
         (erase-buffer))
       (kill-buffer errbuf))))
 
+(defun rufo-minor-mode--should-apply-patch-p (rufo-status-code)
+  "Check if the file needs changes based on `RUFO-STATUS-CODE'."
+  (eq rufo-status-code 3))
+
 (defun rufo-format ()
   "Format the current buffer with rufo."
   (interactive)
@@ -178,6 +182,7 @@
          (coding-system-for-read 'utf-8)
          (coding-system-for-write 'utf-8)
          (rufo-args (rufo-minor-mode--args))
+         (rufo-call-result nil)
          )
     (if (rufo-minor-mode--verify)
         (unwind-protect
@@ -187,13 +192,14 @@
                 (erase-buffer)))
           (with-current-buffer patchbuf
             (erase-buffer))
-          (if rufo-args
-              (apply 'call-process-region (point-min) (point-max) executable nil (list :file outputfile) nil rufo-args)
-            (call-process-region (point-min) (point-max) executable nil (list :file outputfile) nil))
-          (call-process-region nil nil "diff" nil patchbuf nil "-n" "--text" "-" outputfile)
-          (rufo-minor-mode--apply-rcs-patch patchbuf)
-          (message "Applied rufo with args `%s'" rufo-args)
-          (if errbuf (rufo-minor-mode--kill-error-buffer errbuf))))
+          (setq rufo-call-result (if rufo-args
+                                     (apply 'call-process-region (point-min) (point-max) executable nil (list :file outputfile) nil rufo-args)
+                                   (call-process-region (point-min) (point-max) executable nil (list :file outputfile) nil)))
+          (when (rufo-minor-mode--should-apply-patch-p rufo-call-result)
+            (call-process-region nil nil "diff" nil patchbuf nil "-n" "--text" "-" outputfile)
+            (rufo-minor-mode--apply-rcs-patch patchbuf)
+            (message "Applied rufo with args `%s'" rufo-args)
+            (if errbuf (rufo-minor-mode--kill-error-buffer errbuf)))))
       (kill-buffer patchbuf)
       (delete-file errorfile)
       (delete-file outputfile)))
