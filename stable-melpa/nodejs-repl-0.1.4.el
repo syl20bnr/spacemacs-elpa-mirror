fd20b3aa18a7255448a6ca47731f4b7469248d5c
Golem <golem@d12frosted.io>
2017-07-22 04:30:06 +0000

    snapshot 22/07/17 04:30:06

--- stable-melpa/nodejs-repl-0.1.4.el
+++ stable-melpa/nodejs-repl-0.1.4.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2012-2017  Takeshi Arabiki
 
 ;; Author: Takeshi Arabiki
-;; Version: 0.1.1
-;; Package-Version: 0.1.1
+;; Version: 0.1.4
+;; Package-Version: 0.1.4
 
 ;;  This program is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License as published by
@@ -40,7 +40,7 @@
 ;;
 ;;     (add-hook 'js-mode-hook
 ;;               (lambda ()
-;;                 (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-sexp)
+;;                 (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
 ;;                 (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
 ;;                 (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
 ;;                 (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
@@ -54,7 +54,7 @@
   "Run Node.js REPL and communicate the process."
   :group 'processes)
 
-(defconst nodejs-repl-version "0.1.1"
+(defconst nodejs-repl-version "0.1.4"
   "Node.js mode Version.")
 
 (defcustom nodejs-repl-command "node"
@@ -149,6 +149,9 @@ See also `comint-process-echoes'"
    "'[^'\\]*\\(?:\\\\.[^'\\]*\\)*"                ; single quote
    "\\)"
    "$"))
+
+(defvar nodejs-repl-unary-operators
+  '(! + - void typeof delete))
 
 (defvar nodejs-repl-cache-token "")
 (defvar nodejs-repl-cache-candidates ())
@@ -286,6 +289,56 @@ when receive the output string"
       (when (re-search-forward (concat nodejs-repl-prompt nodejs-repl-prompt) end t)
         (replace-match nodejs-repl-prompt)))))
 
+;; cf. https://www.ecma-international.org/ecma-262/#sec-ecmascript-language-expressions
+(defun nodejs-repl--beginning-of-expression ()
+  (search-backward-regexp "[[:graph:]]" nil t)
+  (forward-char)
+  (cond
+   ;; Allow function
+   ((and (eq (char-before) ?})
+         (save-excursion
+           (backward-list)
+           (search-backward-regexp "[[:graph:]]" nil t)
+           (and (eq (char-before) ?=) (eq (char-after) ?>))))
+    (backward-list)
+    (search-backward-regexp "\\(\\w\\|)\\)\\s-*=>" nil t)
+    (forward-char)
+    (nodejs-repl--backward-expression))
+   (t
+    (nodejs-repl--backward-expression)
+    (while (and (not (bobp))
+                (or
+                 (and (eq (char-syntax (char-after)) ?\()
+                      (save-excursion
+                        (search-backward-regexp "[[:graph:]]" nil t)
+                        (and (not (eq (char-after) ?\;))  ; e.g. otherExp; (exp)
+                             (not (eq (sexp-at-point) 'return)))))  ; e.g. return (exp)
+                 (eq (char-before) ?.)  ; e.g. obj.method
+                 (save-excursion
+                   (backward-char)
+                   (eq (sexp-at-point) 'function))))
+      (search-backward-regexp "[[:graph:]]" nil t)
+      (forward-char)
+      (nodejs-repl--backward-expression))
+
+    ;; e.g. !function() {}()
+    (let ((exp (save-excursion
+                 (search-backward-regexp "[[:graph:]]" nil t)
+                 (or (sexp-at-point) (intern (char-to-string (char-after)))))))
+      (when (member exp nodejs-repl-unary-operators)
+       (search-backward (symbol-name exp) nil)))))
+  (point))
+
+(defun nodejs-repl--backward-expression ()
+  (cond
+   ((eq (char-syntax (char-before)) ?\))
+    (backward-list))
+   ((save-excursion
+      (search-backward-regexp "[[:graph:]]" nil t)
+      (eq (char-syntax (char-after)) ?w))
+    (backward-sexp))
+   (t
+    (error "No proper expression is found backward"))))
 
 ;;;--------------------------
 ;;; Public functions
@@ -316,17 +369,20 @@ when receive the output string"
 ;;;###autoload
 (defun nodejs-repl-load-file (file)
   "Load the file to the `nodejs-repl-process'"
-  (interactive (list (read-file-name "Load file: " nil nil 'lambda)))
+  (interactive (list (expand-file-name (read-file-name "Load file: " nil nil 'lambda))))
   (let ((proc (nodejs-repl--get-or-create-process)))
     (comint-send-string proc (format ".load %s\n" file))))
 
 ;;;###autoload
-(defun nodejs-repl-send-last-sexp ()
+(defun nodejs-repl-send-last-expression ()
   "Send the expression before point to the `nodejs-repl-process'"
   (interactive)
-  (nodejs-repl-send-region (save-excursion (backward-sexp)
-                             (point))
+  (nodejs-repl-send-region (save-excursion (nodejs-repl--beginning-of-expression))
                            (point)))
+
+;;;###autoload
+(defun nodejs-repl-send-last-sexp () (interactive))  ;; Dummy definition for autoload
+(define-obsolete-function-alias 'nodejs-repl-send-last-sexp 'nodejs-repl-send-last-expression)
 
 ;;;###autoload
 (defun nodejs-repl-switch-to-repl ()
@@ -414,7 +470,8 @@ otherwise spawn one."
         (format nodejs-repl-prompt-re-format nodejs-repl-prompt nodejs-repl-prompt))
   (setq nodejs-repl-nodejs-version
         ;; "v7.3.0" => "7.3.0", "v7.x-dev" => "7"
-        (replace-regexp-in-string nodejs-repl--nodejs-version-re "\\1" (shell-command-to-string "node --version")))
+        (replace-regexp-in-string nodejs-repl--nodejs-version-re "\\1"
+                                  (shell-command-to-string (concat nodejs-repl-command " --version"))))
   (let* ((repl-mode (or (getenv "NODE_REPL_MODE") "magic"))
          (nodejs-repl-code (format nodejs-repl-code-format
                                    (window-width) nodejs-repl-prompt repl-mode )))
--- stable-melpa/nodejs-repl-readme.txt
+++ stable-melpa/nodejs-repl-readme.txt
@@ -17,7 +17,7 @@ You can define key bindings to send JavaScript codes to REPL like below:
 
     (add-hook 'js-mode-hook
               (lambda ()
-                (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-sexp)
+                (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
                 (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
                 (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
                 (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
@@ -31,7 +31,7 @@ You can define key bindings to send JavaScript codes to REPL like below:
   "Run Node.js REPL and communicate the process."
   :group 'processes)
 
-(defconst nodejs-repl-version "0.1.1"
+(defconst nodejs-repl-version "0.1.4"
   "Node.js mode Version.")
 
 (defcustom nodejs-repl-command "node"
@@ -126,6 +126,9 @@ not support Unicode characters
    "'[^'\\]*\\(?:\\\\.[^'\\]*\\)*"                ; single quote
    "\\)"
    "$"))
+
+(defvar nodejs-repl-unary-operators
+  '(! + - void typeof delete))
 
 (defvar nodejs-repl-cache-token "")
 (defvar nodejs-repl-cache-candidates ())
