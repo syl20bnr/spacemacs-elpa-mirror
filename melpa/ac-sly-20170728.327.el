660b411616e7bf4dc01909f56d8b86e438180fc4
Golem <golem@d12frosted.io>
2017-07-28 16:30:05 +0000

    snapshot 28/07/17 16:30:05

--- melpa/ac-sly-20170728.327.el
+++ melpa/ac-sly-20170728.327.el
@@ -2,10 +2,25 @@
 ;;
 ;; Author: Damian T. Dobroczy\'nski <qoocku@gmail.com>
 ;; URL: https://github.com/qoocku/ac-sly
-;; Package-Version: 20150421.1322
+;; Package-Version: 20170728.327
 ;; Version: DEV
 ;; Package-Requires: ((sly "1.0.0-alpha") (auto-complete "1.4") (cl-lib "0.5"))
-;;
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; Commentary:
 ;; This is direct translation of ac-slime module replacing
 ;; "slime" with "sly". That's it.
