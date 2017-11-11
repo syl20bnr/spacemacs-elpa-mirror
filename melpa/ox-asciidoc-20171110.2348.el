dbb39b3173f1a08eef3c8b3d17bdf9f08adccf9c
Golem <golem@d12frosted.io>
2017-11-11 08:30:05 +0000

    snapshot 11/11/17 08:30:05

--- melpa/ox-asciidoc-20171110.2348.el
+++ melpa/ox-asciidoc-20171110.2348.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yasushi SHOJI <yasushi.shoji@gmail.com>
 ;; URL: https://github.com/yashi/org-asciidoc
-;; Package-Version: 20170622.346
+;; Package-Version: 20171110.2348
 ;; Package-Requires: ((org "8.1"))
 ;; Keywords: org, asciidoc
 
@@ -301,14 +301,21 @@ plist holding contextual information."
 
 (defun org-asciidoc-table (table contents info)
   "Transcode TABLE element into AsciiDoc format."
-  (let ((has-header (org-export-table-has-header-p table info))
-        (width (org-export-read-attribute :attr_asciidoc table :width))
-        (pgwide (org-export-read-attribute :attr_asciidoc table :pgwide)))
-    (concat (format "[width=\"%d%%\",options=\"%s\"]\n"
+  (let* ((width (org-export-read-attribute :attr_asciidoc table :width))
+         (cols (org-export-read-attribute :attr_asciidoc table :cols))
+         (header (and (org-export-table-has-header-p table info) "header"))
+         (pgwide (and (org-export-read-attribute :attr_asciidoc table :pgwide) "pgwide"))
+         (options (remq nil (list header pgwide))))
+    (concat (format "[width=\"%d%%\""
 		    (or (and width (string-to-number width))
-                        org-asciidoc-table-width-in-percent)
-                    (concat (and has-header "header")
-                            (and pgwide ",pgwide")))
+                        org-asciidoc-table-width-in-percent))
+            (and cols
+              (concat ",cols=\"" cols "\""))
+            (and options
+             (concat ",options=\""
+                     (string-join options ",")
+                     "\""))
+            "]\n"
 	    "|====\n"
 	    contents
 	    "|====")))
