b4158d29e554d13a5f108409e4ac0ceae23735d0
Golem <golem@d12frosted.io>
2017-11-24 08:30:06 +0000

    snapshot 24/11/17 08:30:06

--- stable-melpa/plain-theme-8.el
+++ stable-melpa/plain-theme-8.el
@@ -1,25 +1,8 @@
 ;;; plain-theme.el --- Plain theme without syntax highlighting
 
-;; Copyright (C) 2016 Yegor Timoshenko
-
-;; Author: Yegor Timoshenko <yegortimoshenko@gmail.com>
-;; URL: https://github.com/yegortimoshenko/plain-theme
-;; Package-Version: 7
-;; Version: 7
-
-;; Permission to use, copy, modify, and/or distribute this software for any
-;; purpose with or without fee is hereby granted, provided that the above
-;; copyright notice and this permission notice appear in all copies.
-
-;; THE SOFTWARE IS PROVIDED AS IS AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-;; REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
-;; FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-;; INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-;; LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-;; OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-;; PERFORMANCE OF THIS SOFTWARE.
-
-;;; Code:
+;; Package-Requires: ((emacs "24"))
+;; Package-Version: 8
+;; Package-X-Original-Version: 8
 
 (deftheme plain "Plain theme without syntax highlighting.")
 
