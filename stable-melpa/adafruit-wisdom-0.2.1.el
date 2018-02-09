597c5ef4987fa220fe388906bd6fea43ad10bfad
Golem <golem@d12frosted.io>
2018-02-09 08:30:06 +0000

    snapshot 09/02/18 08:30:06

--- stable-melpa/adafruit-wisdom-0.2.1.el
+++ stable-melpa/adafruit-wisdom-0.2.1.el
@@ -1,11 +1,11 @@
 ;;; adafruit-wisdom.el --- Get/display adafruit.com quotes
 
 ;; Author: Neil Okamoto <neil.okamoto+melpa@gmail.com>
-;; Version: 0.2
-;; Package-Version: 0.2.0
+;; Version: 0.2.1
+;; Package-Version: 0.2.1
 ;; Keywords: games
 ;; URL: https://github.com/gonewest818/adafruit-wisdom.el
-;; Package-Requires: ((emacs "24"))
+;; Package-Requires: ((emacs "25"))
 
 ;; This program is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the
@@ -29,7 +29,8 @@
 
 ;;; Code:
 
-(require 'url-vars)
+(require 'dom)
+(require 'url)
 (require 'xml)
 
 (defconst adafruit-wisdom-quote-url "https://www.adafruit.com/feed/quotes.xml"
@@ -56,19 +57,19 @@ Returns the parsed XML."
         (url-insert-file-contents adafruit-wisdom-quote-url)
         (xml-parse-region (point-min) (point-max))))))
 
+;;;###autoload
 (defun adafruit-wisdom-select ()
-  "Select a quote at random."
-  (let* ((root  (adafruit-wisdom-cached-get))
-         ;; parse assuming the following RSS format:
-         ;; ((rss (channel (item ...) (item ...) (item ...) ...)))
-         ;; where each item contains (item (title nil "the quote") ...)
-         ;; and we need just "the quote"
-         (rss   (car root))
-         (chan  (car (xml-get-children rss 'channel)))
-         (items (xml-get-children chan 'item))
+  "Select a quote at random and return as a string.
+
+Parse assuming the following RSS format:
+     ((rss (channel (item ...) (item ...) (item ...) ...)))
+where each item contains:
+     (item (title nil \"the quote\") ...)
+and we  need just \"the quote\"."
+  (let* ((items (dom-by-tag (adafruit-wisdom-cached-get) 'item))
          (pick  (nth (random (length items)) items))
-         (title (car (xml-get-children pick 'title))))
-    (car (last title))))
+         (title (dom-text (car (dom-by-tag pick 'title)))))
+    title))
 
 ;;;###autoload
 (defun adafruit-wisdom (&optional insert)
