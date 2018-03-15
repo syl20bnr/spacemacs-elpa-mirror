ca458a250b85c3926401f8e166adae1ee873f6f7
Golem <golem@d12frosted.io>
2018-03-15 08:30:10 +0000

    snapshot 15/03/18 08:30:10

--- stable-melpa/fountain-mode-2.5.1.el
+++ stable-melpa/fountain-mode-2.5.1.el
@@ -4,8 +4,8 @@
 
 ;; Author: Paul Rankin <hello@paulwrankin.com>
 ;; Keywords: wp
-;; Package-Version: 2.5.0
-;; Version: 2.5.0
+;; Package-Version: 2.5.1
+;; Version: 2.5.1
 ;; Package-Requires: ((emacs "24.5"))
 ;; URL: https://github.com/rnkn/fountain-mode
 
@@ -141,7 +141,7 @@
 ;;; Code:
 
 (defconst fountain-version
-  "2.5.0")
+  "2.5.1")
 
 (defun fountain-version ()
   "Return `fountain-mode' version."
@@ -282,6 +282,15 @@
 
 (define-obsolete-variable-alias 'fountain-export-buffer-name
   'fountain-export-tmp-buffer-name "2.4.0")
+
+(define-obsolete-variable-alias 'fountain-endnotes-buffer-name
+  'fountain-endnotes-buffer "2.5.1")
+
+(make-obsolete-variable 'fountain-endnotes-window-side
+                        'fountain-endnotes-display-alist "2.5.1")
+
+(make-obsolete-variable 'fountain-endnotes-window-size
+                        'fountain-endnotes-display-alist "2.5.1")
 
 
 ;;; Customization
@@ -783,6 +792,11 @@ To switch between these levels, customize the value of
   :prefix "fountain-"
   :link '(info-link "(emacs) Font Lock")
   :group 'fountain)
+
+(defface fountain
+  '((t nil))
+  "Default base-level face for `fountain-mode' buffers."
+  :group 'fountain-faces)
 
 (defface fountain-action
   '((t nil))
@@ -2319,6 +2333,7 @@ specify a different filename."
      :template fountain-export-tex-template
      :string-replace (("%" "\\\\%")
                       ("&" "\\\\&")
+                      ("#" "\\\\#")
                       ("\\$" "\\\\$")
                       ("\\*\\*\\*\\(.+?\\)\\*\\*\\*" "\\\\textbf{\\\\emph{\\1}}")
                       ("\\*\\*\\(.+?\\)\\*\\*" "\\\\textbf{\\1}")
@@ -3557,11 +3572,7 @@ Used by `fountain-outline-cycle'.")
 
 (defcustom fountain-outline-startup-level
   0
-  "Outline level to show when visiting a file.
-
-This can be set on a per-file basis by including in metadata:
-
-\tstartup-level: N"
+  "Outline level to show when visiting a file."
   :type '(choice (const :tag "Show all" 0)
                  (const :tag "Show top-level" 1)
                  (const :tag "Show scene headings" 6)
@@ -3797,6 +3808,21 @@ data reflects `outline-regexp'."
          (string-width (match-string 2)))
         (t 6)))
 
+(defun fountain-outline-to-indirect-buffer ()
+  (interactive)
+  (let (beg end heading-name)
+    (save-excursion
+      (outline-back-to-heading t)
+      (setq beg (point))
+      (if (or (fountain-match-section-heading)
+              (fountain-match-scene-heading))
+          (setq heading-name (match-string-no-properties 3)))
+      (outline-end-of-subtree)
+      (setq end (point)))
+    (clone-indirect-buffer heading-name t)
+    (narrow-to-region beg end)
+    (outline-show-all)))
+
 
 ;;; Navigation
 
@@ -3944,7 +3970,7 @@ WARNING: if using other Fountain apps, check to make sure they
 support endnotes."
   :group 'fountain)
 
-(defcustom fountain-endnotes-buffer-name
+(defcustom fountain-endnotes-buffer
   "%s<endnotes>"
   "Name of buffer in which to display file endnotes.
 `%s' is replaced with `buffer-name'.
@@ -3953,45 +3979,28 @@ To hide this buffer from the buffer list, prefix with a space."
   :type 'string
   :group 'fountain-endnotes)
 
+(defcustom fountain-endnotes-display-alist
+  '((side . right)
+    (window-width . 40)
+    (slot . 1))
+  "Alist used to display endnotes buffer.
+
+See `display-buffer-in-side-window' for example options."
+  :type 'alist
+  :group 'fountain-endnotes)
+
 (defcustom fountain-endnotes-select-window
   nil
   "If non-nil, switch to endnotes window upon displaying it."
   :type 'boolean
   :group 'fountain-endnotes)
 
-(defcustom fountain-endnotes-window-side
-  'right
-  "Preferred side of frame to display endnotes window."
-  :type '(choice (const :tag "Left" left)
-                 (const :tag "Right" right)
-                 (const :tag "Top" top)
-                 (const :tag "Bottom" bottom))
-  :group 'fountain-endnotes)
-
-(defcustom fountain-endnotes-window-size
-  '(0.3 0.25)
-  "Height and width of the endnotes window as a fraction of root window."
-  :type '(list (float :tag "Height")
-               (float :tag "Width"))
-  :group 'fountain-endnotes)
-
-;; (defcustom fountain-endnotes-display-function
-;;   'display-buffer-pop-up-window
-;;   "Buffer display function used to display endnotes."
-;;   :type '(radio (const :tag "Pop-up new window" display-buffer-pop-up-window)
-;;                 (const :tag "Pop-up new frame" display-buffer-pop-up-frame)
-;;                 (const :tag "Show in same window" display-buffer-same-window))
-;;   :group 'fountain-endnotes)
-
 (defun fountain-show-or-hide-endnotes ()
   "Pop up a window containing endnotes of current buffer.
 
 Display a window containing an indirect clone of the current
 buffer, narrowed to the first endnotes page break to the end of
-buffer.
-
-The window displayed is a special \"side\" window, which will
-persist even when calling \\[delete-other-windows]."
+buffer."
   (interactive)
   (set-buffer (or (buffer-base-buffer) (current-buffer)))
   (save-excursion
@@ -4000,23 +4009,20 @@ persist even when calling \\[delete-other-windows]."
       (goto-char (point-min))
       (let ((beg (if (re-search-forward fountain-end-regexp nil t)
                      (point)))
-            (src (current-buffer))
-            (buf (format fountain-endnotes-buffer-name (buffer-name))))
+            (buffer (current-buffer))
+            (endnotes-buffer (format fountain-endnotes-buffer (buffer-name))))
         (if beg
-            (if (get-buffer-window buf (selected-frame))
-                (delete-windows-on buf (selected-frame))
+            (if (get-buffer-window endnotes-buffer (selected-frame))
+                (delete-windows-on endnotes-buffer (selected-frame))
               (display-buffer-in-side-window
-               (or (get-buffer buf)
-                   (make-indirect-buffer src buf t))
-               (list (cons 'inhibit-same-window t)
-                     (cons 'side fountain-endnotes-window-side)
-                     (cons 'window-height (car fountain-endnotes-window-size))
-                     (cons 'window-width (cadr fountain-endnotes-window-size))))
-              (with-current-buffer buf
+               (or (get-buffer endnotes-buffer)
+                   (make-indirect-buffer buffer endnotes-buffer t))
+               fountain-endnotes-display-alist)
+              (with-current-buffer endnotes-buffer
                 (narrow-to-region (1+ beg) (point-max)))
               (if fountain-endnotes-select-window
-                  (select-window (get-buffer-window buf (selected-frame))))
-              (message "Showing `%s' endnotes; %s to hide" src
+                  (select-window (get-buffer-window endnotes-buffer (selected-frame))))
+              (message "Showing `%s' endnotes; %s to hide" (buffer-name buffer)
                        (key-description (where-is-internal this-command
                                                            overriding-local-map t))))
           (user-error "Buffer `%s' does not contain endnotes" (buffer-name)))))))
@@ -4879,6 +4885,7 @@ keywords suitable for Font Lock."
     (define-key map (kbd "C-c TAB") #'fountain-outline-cycle)
     (define-key map (kbd "<backtab>") #'fountain-outline-cycle-global)
     (define-key map (kbd "S-TAB") #'fountain-outline-cycle-global)
+    (define-key map (kbd "C-c C-x b") #'fountain-outline-to-indirect-buffer)
     ;; Pages
     (define-key map (kbd "C-c C-x p") #'fountain-count-pages)
     ;; Endnotes:
@@ -4915,6 +4922,8 @@ keywords suitable for Font Lock."
     ("Outline"
      ["Cycle Scene/Section Visibility" fountain-outline-cycle]
      ["Cycle Global Visibility" fountain-outline-cycle-global]
+     "---"
+     ["Open Scene/Section in Indirect Buffer" fountain-outline-to-indirect-buffer]
      "---"
      ["Up Heading" fountain-outline-up]
      ["Next Heading" fountain-outline-next]
@@ -5045,7 +5054,7 @@ keywords suitable for Font Lock."
      ["Toggle Auto-Upcasing" (customize-set-variable 'fountain-tab-command 'fountain-toggle-auto-upcase)
       :style radio
       :selected (eq fountain-tab-command 'fountain-toggle-auto-upcase)]
-     ["Auto-Complete" (customize-set-variable 'fountain-tab-command 'completion-at-point)
+     ["Auto-Completion" (customize-set-variable 'fountain-tab-command 'completion-at-point)
       :style radio
       :selected (eq fountain-tab-command 'completion-at-point)])
     ["Display Elements Auto-Aligned"
@@ -5088,14 +5097,11 @@ keywords suitable for Font Lock."
     (if unsaved (custom-save-all))))
 
 
-;;; Syntax Table
+;;; Mode Definition
 
 (defvar fountain-mode-syntax-table
   (make-syntax-table)
   "Syntax table for `fountain-mode'.")
-
-
-;;; Mode Definition
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.fountain\\'" . fountain-mode))
@@ -5105,10 +5111,8 @@ keywords suitable for Font Lock."
   "Major mode for screenwriting in Fountain markup."
   :group 'fountain
   (fountain-init-vars)
-  (let ((n (plist-get (fountain-read-metadata) 'startup-level)))
-    (if (stringp n)
-        (setq-local fountain-outline-startup-level
-                    (min (string-to-number n) 6))))
+  (hack-local-variables)
+  (face-remap-add-relative 'default 'fountain)
   (add-hook 'post-command-hook #'fountain-set-edit-line nil t)
   (add-hook 'post-command-hook #'fountain-auto-upcase-deactivate-maybe nil t)
   (add-hook 'post-self-insert-hook #'fountain-auto-upcase nil t)
