7e32d21347750af3680c289e54e8a5c3f8f108f8
Golem <golem@d12frosted.io>
2018-03-31 00:30:09 +0000

    snapshot 31/03/18 00:30:09

--- gnu/num3-mode-1.3.el
+++ gnu/num3-mode-1.3.el
@@ -1,11 +1,11 @@
 ;;; num3-mode.el --- highlight groups of digits in long numbers  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2012 Free Software Foundation, Inc.
+;; Copyright (C) 2012-2017 Free Software Foundation, Inc.
 
 ;; Author: Felix Lee <felix8a@gmail.com>, Michal Nazarewicz <mina86@mina86.com>
 ;; Maintainer: Michal Nazarewicz <mina86@mina86.com>
 ;; Keywords: faces, minor-mode
-;; Version: 1.2
+;; Version: 1.3
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -40,15 +40,16 @@
 
 (defgroup num3 nil
   "Num3 is a minor mode that makes long numbers more readable by
-highlighting groups of decimal digits or 4 hex digits when
-font-lock is on."
+highlighting groups of digits in a number."
   :group 'text)
 
 (defcustom num3-group-size 3
-  "Number of digits to group in decimal numbers.")
+  "Number of digits to group in decimal numbers."
+  :type 'integer)
 
 (defcustom num3-threshold 5
-  "Number must be at least that long to start highlighting.")
+  "Number must be at least that long to start highlighting."
+  :type 'integer)
 
 (defface num3-face-odd
   '((t))
@@ -68,41 +69,51 @@ addition to any other font-lock highlighting."
 (define-minor-mode num3-mode
   "Toggle num3 minor mode in the current buffer.
 Num3 minor mode makes long numbers more readable by highlighting
-groups of digits when font-lock mode is on.
+groups of digits when font-lock mode is enabled.
 
 If a number is longer than `num3-threshold', the mode will split
 it into a group of `num3-group-size' (if number is decimal) or
-4 (if number is hexadecimal) digits.  Hexadecimal number is
-detected as one starting with 0x, 0X or #.
+four (if number is hexadecimal or binary) digits.
 
-With decimal numbers, fractions are recognised as well and
-grouped from the beginning rathar then from end.  For instance,
-with group size of 3, a number \"12345.12345\" will be split into
-groups as follows: \"12|345.123|45\".  Fractions without integer
-part are also recognised, eg. \".12345\".
+Hexadecimal numbers are recognised by \"0x\" or \"#x\"
+prefix (case insensitive) and binary numbers by \"0b\" or \"#b\"
+prefix.  (There is no special handling for octal numbers –
+starting with \"0o\" or \"#o\" – and instead they are handled
+like decimal numbers).
 
-The groups are highlighted alternately using `num3-face-odd' and
+Decimal fractions are recognised as well and grouped from the
+beginning rathar then the end.  For instance, with group size of
+three, a number \"12345.12345\" will be split into groups as
+follows: \"12|345.123|45\".  Fractions without integer part are
+also recognised, eg. \".12345\".
+
+Groups are highlighted alternately using `num3-face-odd' and
 `num3-face-even' faces.  `num3-face-odd' face (which is empty by
-default) is the one used for the group closest to the decimal point,
-ie. groups are counted starting with one outwards from the (place
-where) decimal point (would be) is."
+default) is the one used for the group closest to the decimal
+point, i.e. groups are counted starting with one outwards from
+the (place where) decimal point (would be) is."
   nil " num3" nil
   (if num3-mode
       (unless (assoc 'num3--matcher font-lock-keywords)
         (font-lock-add-keywords nil '(num3--matcher) 'append))
     (font-lock-remove-keywords nil '(num3--matcher)))
-  (when font-lock-mode
-    (font-lock-fontify-buffer)))
+  (if (fboundp 'font-lock-flush) (font-lock-flush)
+    (when font-lock-mode (with-no-warnings (font-lock-fontify-buffer)))))
 
 ;;;###autoload
 (define-globalized-minor-mode global-num3-mode num3-mode num3-mode)
 
 (defconst num3--number-re
+  ;; Hexadecimal and binary are both using the first capture group because we
+  ;; group them both in four-digit groups.  There’s no explicit support for
+  ;; octal numbers because we just use logic for a decimal number, i.e. the same
+  ;; grouping.
   (concat "[0#][xX]\\([[:xdigit:]]+\\)"       ; 1 = hexadecimal
-        "\\|\\(?1:\\b\\(?:[0-9]+[a-fA-F]\\|"  ; 1 = hexadecimal
+       "\\|[0#][bB]\\(?1:[01]+\\)"            ; 1 = binary
+       "\\|\\(?1:\\b\\(?:[0-9]+[a-fA-F]\\|"   ; 1 = hexadecimal w/o prefix
                  "[a-fA-F]+[0-9]\\)[[:xdigit:]]*\\b\\)"
-        "\\|\\([0-9]+\\)"                     ; 2 = decimal
-        "\\|\\.\\([0-9]+\\)"))                ; 3 = fraction
+       "\\|\\([0-9]+\\)"                      ; 2 = decimal
+       "\\|\\.\\([0-9]+\\)"))                 ; 3 = fraction
 
 (defun num3--matcher (lim)
   "Function used as a font-lock-keywoard handler used in `num3-mode'.
@@ -149,6 +160,51 @@ non-nil.  The region the face is set to is from LO to HI."
 
 ;;;; ChangeLog:
 
+;; 2018-03-30  Michal Nazarewicz  <mina86@mina86.com>
+;; 
+;; 	num3-mode: cut new release version 1.3
+;; 
+;; 2017-05-19  Michal Nazarewicz  <mina86@mina86.com>
+;; 
+;; 	num3-mode: support binary numbers
+;; 
+;; 	It maks more sense for binary numbers to be grouped in nibbles rather 
+;; 	than threes (which is the default for decimal numbers) so if number 
+;; 	starts with 0b or #b, treat it as a binary number and group in fours.
+;; 
+;; 	Grouping in eights (i.e., a full byte) would also make sense but 
+;; 	piggy-backing on hexadecimal number support is much easier. :P
+;; 
+;; 	* /packages/num3-mode/num3-mode.el (num3--number-re): Add alternative 
+;; 	for matching binary numbers.
+;; 	(num3-mode): Document that binary numbers are also supported plus some
+;; 	other minor changes.
+;; 
+;; 2016-06-14  Michal Nazarewicz  <mina86@mina86.com>
+;; 
+;; 	* packages/num3-mode/num3-mode.el: Fix compilation warnings.
+;; 
+;; 	Fixes the following warnings:
+;; 
+;; 	In toplevel form: num3-mode.el:47:1:Warning: defcustom for
+;; 	‘num3-group-size’ fails to specify
+;; 	   type num3-mode.el:47:1:Warning: defcustom for ‘num3-group-size’ fails
+;; 	to specify
+;; 	   type num3-mode.el:50:1:Warning: defcustom for ‘num3-threshold’ fails
+;; 	to specify
+;; 	   type num3-mode.el:50:1:Warning: defcustom for ‘num3-threshold’ fails
+;; 	to specify
+;; 	   type
+;; 
+;; 	In num3-mode: num3-mode.el:95:11:Warning: ‘font-lock-fontify-buffer’ is
+;; 	for interactive use
+;; 	   only; use ‘font-lock-ensure’ or ‘font-lock-flush’ instead.
+;; 
+;; 2014-10-15  Stefan Monnier  <monnier@iro.umontreal.ca>
+;; 
+;; 	* packages/num3-mode/num3-mode.el (num3-mode): Use font-lock-flush if
+;; 	available.
+;; 
 ;; 2014-02-24  Michal Nazarewicz  <mina86@mina86.com>
 ;; 
 ;; 	* num3-mode.el: Catch strings looking like hex number even w/o 0x
