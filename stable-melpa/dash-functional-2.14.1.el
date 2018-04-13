8d68dfee8ba789662071e3d24369431d48db827b
Golem <golem@d12frosted.io>
2018-04-13 08:30:05 +0000

    snapshot 13/04/18 08:30:05

--- stable-melpa/dash-functional-2.14.1.el
+++ stable-melpa/dash-functional-2.14.1.el
@@ -5,7 +5,7 @@
 ;; Authors: Matus Goljer <matus.goljer@gmail.com>
 ;;          Magnar Sveen <magnars@gmail.com>
 ;; Version: 1.2.0
-;; Package-Version: 2.14.0
+;; Package-Version: 2.14.1
 ;; Package-Requires: ((dash "2.0.0") (emacs "24"))
 ;; Keywords: lisp functions combinators
 
