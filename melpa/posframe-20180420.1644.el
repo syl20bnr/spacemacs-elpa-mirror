a64d6dfa0cf4a2c3917b760202ab0d4d68ea04f0
Golem <golem@d12frosted.io>
2018-04-21 00:30:06 +0000

    snapshot 21/04/18 00:30:06

--- melpa/posframe-20180420.1644.el
+++ melpa/posframe-20180420.1644.el
@@ -5,7 +5,7 @@
 ;; Author: Feng Shu <tumashu@163.com>
 ;; Maintainer: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/posframe
-;; Package-Version: 20180308.1627
+;; Package-Version: 20180420.1644
 ;; Version: 0.3.0
 ;; Keywords: tooltip
 ;; Package-Requires: ((emacs "26"))
@@ -112,7 +112,7 @@
 (defvar-local posframe--frame nil
   "Record posframe's frame.")
 
-(defvar-local posframe--last-position nil
+(defvar-local posframe--last-posframe-pixel-position nil
   "Record the last pixel position of posframe's frame.")
 
 (defvar-local posframe--last-posframe-size nil
@@ -185,7 +185,7 @@ This posframe's buffer is POSFRAME-BUFFER."
                    (equal posframe--last-args args))
         (posframe-delete-frame posframe-buffer)
         (setq-local posframe--last-args args)
-        (setq-local posframe--last-position nil)
+        (setq-local posframe--last-posframe-pixel-position nil)
         (setq-local posframe--last-posframe-size nil)
         (setq-local posframe--frame
                     (make-frame
@@ -443,13 +443,13 @@ WIDTH and MIN-WIDTH."
                                               parent-frame-height)
   "Move POSFRAME to POSITION.
 This need PARENT-FRAME-WIDTH and PARENT-FRAME-HEIGHT"
-  (unless (and (equal position posframe--last-position)
+  (unless (and (equal position posframe--last-posframe-pixel-position)
                ;; When working frame's size change, re-posit
                ;; the posframe.
                (equal posframe--last-parent-frame-size
                       (cons parent-frame-width parent-frame-height)))
     (set-frame-position posframe (car position) (cdr position))
-    (setq-local posframe--last-position position)
+    (setq-local posframe--last-posframe-pixel-position position)
     (setq-local posframe--last-parent-frame-size
                 (cons parent-frame-width parent-frame-height)))
   ;; Make posframe's posframe--frame visible
