7c3c59274cb2e74dc0a5c98952b116b605d98d3b
Golem <golem@d12frosted.io>
2018-05-19 20:30:05 +0000

    snapshot 19/05/18 20:30:05

--- melpa/anaconda-mode-20180519.637.el
+++ melpa/anaconda-mode-20180519.637.el
@@ -4,8 +4,8 @@
 
 ;; Author: Artem Malyshev <proofit404@gmail.com>
 ;; URL: https://github.com/proofit404/anaconda-mode
-;; Package-Version: 20180512.1531
-;; Version: 0.1.10
+;; Package-Version: 20180519.637
+;; Version: 0.1.11
 ;; Package-Requires: ((emacs "24") (pythonic "0.1.0") (dash "2.6.0") (s "1.9") (f "0.16.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -94,14 +94,14 @@
 
 ;;; Server.
 
-(defvar anaconda-mode-server-version "0.1.10"
+(defvar anaconda-mode-server-version "0.1.11"
   "Server version needed to run `anaconda-mode'.")
 
 (defvar anaconda-mode-server-command "
 import sys, site
 site.addsitedir('.')
 import anaconda_mode
-anaconda_mode.main(sys.argv[1:])
+anaconda_mode.main(sys.argv[-2:])
 " "Run `anaconda-mode' server.")
 
 (defvar anaconda-mode-process-name "anaconda-mode"
