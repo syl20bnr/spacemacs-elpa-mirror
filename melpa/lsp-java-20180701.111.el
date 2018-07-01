b07d784d3f9f6b63aaf15bbfd3162ca3bcdeac67
Golem <golem@d12frosted.io>
2018-07-01 12:30:04 +0000

    snapshot 01/07/18 12:30:04

--- melpa/lsp-java-20180701.111.el
+++ melpa/lsp-java-20180701.111.el
@@ -1,7 +1,7 @@
 ;;; lsp-java.el --- Java support for lsp-mode
 
 ;; Version: 1.0
-;; Package-Version: 20180623.223
+;; Package-Version: 20180701.111
 ;; Package-Requires: ((emacs "25.1") (lsp-mode "3.0"))
 ;; Keywords: java
 ;; URL: https://github.com/emacs-lsp/lsp-java
@@ -211,6 +211,11 @@ A package or type name prefix (e.g. 'org.eclipse') is a valid entry. An import i
   :group 'lsp-java
   :type 'boolean)
 
+(defcustom lsp-java-completion-guess-arguments nil
+  "When set to true, method arguments are guessed when a method is selected from as list of code assist proposals."
+  :group 'lsp-java
+  :type 'boolean)
+
 (defun lsp-java--json-bool (param)
   "Return a PARAM for setting parsable by json.el for booleans."
   (or param :json-false))
@@ -257,7 +262,8 @@ A package or type name prefix (e.g. 'org.eclipse') is a valid entry. An import i
       (enabled . ,(lsp-java--json-bool lsp-java-auto-build)))
      (completion
       (favoriteStaticMembers . ,lsp-java-favorite-static-members)
-      (importOrder . ,lsp-java-import-order)))))
+      (importOrder . ,lsp-java-import-order)
+      (guessMethodArguments . ,lsp-java-completion-guess-arguments)))))
 
 (defun lsp-java--locate-server-jar ()
   "Return the jar file location of the language server.
